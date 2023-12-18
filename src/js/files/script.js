0502cf78379dde1a9085d72f30568e3fc588b13f
Sudzuya <dima32965@gmail.com>
2023-12-19 00:32:56 +0400

    Первая версия сайта

---
+++ src/js/files/script.js
@@ -0,0 +1,8 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+import { isMobile } from "./functions.js";
+// Підключення списку активних модулів
+import { flsModules } from "./modules.js";
+
+
+let dataImages = document.querySelector('[data-images]')
+
